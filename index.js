d823400987a94cf7ae51662dde57990eb73ec1d5
Cross <we5705988@gmail.com>
2023-02-08 22:35:14 -0300

    Hiponetusa em JavaScript

---
+++ index.js
@@ -0,0 +1,13 @@
+let a;
+let b;
+let c;
+
+a = window.prompt("Enter side A");
+a = Number(a);
+a = window.prompt("Enter side B");
+b = Number(b);
+
+c = Math.pow(a, 2) + Math.pow(b, 2);
+c = Math.sqrt(c);
+
+console.log("Side C: ", c);
